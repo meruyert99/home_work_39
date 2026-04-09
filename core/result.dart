b9255386b73d2f821f8b64f0520ad26e2e514d58
meruyert99 <kozhahmetmikalek@gmail.com>
2026-04-09 10:17:16 +0500

    Create result.dart

---
+++ core/result.dart
@@ -0,0 +1,9 @@
+class Result<T> {
+  final T? data;
+  final String? error;
+
+  Result.success(this.data) : error = null;
+  Result.failure(this.error) : data = null;
+
+  bool get isSuccess => data != null;
+}
